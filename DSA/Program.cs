05408769e4fb8a1bd6553ebc8d801689a0ac2d56
John Mondragon <raskaman@msn.com>
2025-03-27 13:39:06 +0900

    using a value tuple example

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace DSA
+﻿using Statistics = (int Min, int Max, double Avg);
+
+namespace DSA
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Statistics result = Calculate(4, 8, 13);
+            Console.WriteLine($"Min = {result.Min} / Max = {result.Max} / Avg = {result.Avg:F2}");
+
+
+        }
+        static Statistics Calculate(params int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return (0, 0, 0);
+            }
+
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+                min = (number < min) ? number : min;
+                max = (number > max) ? number : max;
+            }
+
+            return (min, max, (double)sum/numbers.Length);
         }
     }
+
+   
+    
 }
