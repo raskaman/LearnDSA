5e07dba5e235e137d34f426a3afece5824de234a
John Mondragon <raskaman@msn.com>
2025-03-28 12:11:34 +0900

    adding month names, tunning code without program declarations

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,40 +1,47 @@
 ﻿using Statistics = (int Min, int Max, double Avg);
 
-namespace DSA
+Console.WriteLine("Hello, World!");
+
+Statistics result = Calculate(4, 8, 13);
+Console.WriteLine($"Min = {result.Min} / Max = {result.Max} / Avg = {result.Avg:F2}");
+Price regularPrice = new Price(100, CurrencyEnum.USD);
+Console.WriteLine(regularPrice);
+Price discountedPrice = regularPrice with { Amount = 80 };
+Console.WriteLine(discountedPrice);
+
+
+static Statistics Calculate(params int[] numbers)
 {
-    internal class Program
+    if (numbers.Length == 0)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-
-            Statistics result = Calculate(4, 8, 13);
-            Console.WriteLine($"Min = {result.Min} / Max = {result.Max} / Avg = {result.Avg:F2}");
-
-
-        }
-        static Statistics Calculate(params int[] numbers)
-        {
-            if (numbers.Length == 0)
-            {
-                return (0, 0, 0);
-            }
-
-            var min = int.MaxValue;
-            var max = int.MinValue;
-            int sum = 0;
-
-            foreach (int number in numbers)
-            {
-                sum += number;
-                min = (number < min) ? number : min;
-                max = (number > max) ? number : max;
-            }
-
-            return (min, max, (double)sum/numbers.Length);
-        }
+        return (0, 0, 0);
     }
 
-   
-    
+    var min = int.MaxValue;
+    var max = int.MinValue;
+    int sum = 0;
+
+    foreach (int number in numbers)
+    {
+        sum += number;
+        min = (number < min) ? number : min;
+        max = (number > max) ? number : max;
+    }
+
+    return (min, max, (double)sum / numbers.Length);
+}
+
+public readonly struct Price(decimal Amount, CurrencyEnum Currency)
+{
+    public readonly decimal Amount { get; init; } = Amount;
+    public readonly CurrencyEnum Currency { get; init; } = Currency;
+
+    public override string ToString() => $"{Amount} {Currency}";
+}
+
+public enum CurrencyEnum
+{
+    USD,
+    EUR,
+    GBP
 }
