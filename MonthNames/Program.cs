00325752d14faefc3067d81ce39afbaa2ec290e9
John Mondragon <raskaman@msn.com>
2025-04-01 18:27:26 +0900

    solution setup, month names example

--- MonthNames/Program.cs
+++ MonthNames/Program.cs
@@ -1,29 +1,20 @@
 ﻿using System.Globalization;
 
-namespace MonthNames
+// MONTH NAMES
+// Chapter 3 (Arrays and Sorting)
+// C# Data Structures and Algorithms, Second Edition
+
+CultureInfo culture = new("es");
+string[] months = new string[12];
+for (int month = 1; month <= 12; month++)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // MONTH NAMES
-            // Chapter 3 (Arrays and Sorting)
-            // C# Data Structures and Algorithms, Second Edition
-
-            CultureInfo culture = new("en");
-            string[] months = new string[12];
-            for (int month = 1; month <= 12; month++)
-            {
-                DateTime firstDay = new(DateTime.Now.Year, month, 1);
-                string name = firstDay.ToString("MMMM", culture);
-                months[month - 1] = name;
-            }
-
-            foreach (string m in months)
-            {
-                Console.WriteLine(m);
-            }
-
-        }
-    }
+    DateTime firstDay = new(DateTime.Now.Year, month, 1);
+    string name = firstDay.ToString("MMMM", culture);
+    months[month - 1] = name;
 }
+
+foreach (string m in months)
+{
+    Console.WriteLine(m);
+}
+
