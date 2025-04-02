3c635b600e2e11db7bb470deb2ef66fd1289f73a
John Mondragon <raskaman@msn.com>
2025-04-02 12:02:01 +0900

    adding insertion sort algorithm

---
+++ 03-SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SortingAlgorithms
+{
+    public static class InsertionSort
+    {
+        public static void Sort(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && a[j] < a[j - 1])
+                {
+
+                    Console.WriteLine(string.Join("| ", a));
+                    (a[j], a[j - 1]) = (a[j - 1], a[j]);
+                    j--;
+                }
+            }
+        }
+    }
+}
