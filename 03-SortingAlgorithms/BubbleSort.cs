efd7dd8c4b6ffa186fe9697933c582c16c4c2ce4
John Mondragon <raskaman@msn.com>
2025-04-02 12:02:34 +0900

    adding selection sort algorithm

---
+++ 03-SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SortingAlgorithms
+{
+    public static class BubbleSort
+    {
+        public static void Sort(int[] a)
+        {
+            var stepCount = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                bool isAnyChange = false;
+                for (int j = 0; j < a.Length -1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        isAnyChange = true;
+                        (a[j], a[j + 1]) = (a[j + 1], a[j]);
+                    }
+                    stepCount += 1;
+                    Console.WriteLine($"step {stepCount}, {string.Join("| ", a)}");
+                    
+                }
+                if (!isAnyChange) { break; }
+            }
+        }
+    }
+}
