c0d8e19c20e1868f89324cf8fce65690934d9ef8
John Mondragon <raskaman@msn.com>
2025-04-02 12:01:43 +0900

    adding selection sort algorithm

---
+++ 03-SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿
+namespace _03_SortingAlgorithms
+{
+   public static class SelectionSort
+    {
+        public static void Sort(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int minIndex = i;
+                int minValue = a[i];
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] < minValue)
+                    {
+                        minIndex = j;
+                        minValue = a[j];
+                    }
+
+                }
+                if (minIndex == i)
+                {
+                    continue;
+                }
+                Console.WriteLine(string.Join("| ", a));
+                (a[i], a[minIndex]) = (a[minIndex], a[i]);
+            }
+        }
+
+        
+    }
+}
