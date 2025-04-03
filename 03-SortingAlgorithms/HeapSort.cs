cb1224333e1b61118f0727f3494b82ebe1a38264
John Mondragon <raskaman@msn.com>
2025-04-03 17:23:06 +0900

    adding heap sort algorithm

---
+++ 03-SortingAlgorithms/HeapSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_SortingAlgorithms
+{
+    public static class HeapSort
+    {
+        public static void Sort(int[] a)
+        {
+            Console.WriteLine($"Entry, {string.Join("| ", a)}");
+            for (int i = a.Length / 2 - 1; i >= 0; i--) // iterate each node that is not a leaf on the binary heap
+            {
+                Heapify(a, a.Length, i); // max-heap the binary heap
+            }
+            Console.WriteLine($"Heapify, {string.Join("| ", a)}");
+
+            for (int i = a.Length -1; i > 0; i--)
+            {
+                (a[0], a[i]) = (a[i], a[0]); // swaps the root element, the max value element with the last element
+                Console.WriteLine($"Swap & remove {string.Join("| ", a)}");
+                Heapify(a, i, 0); // builds a max-heap for the binary heap for remaining element, until there is a least one element
+                Console.WriteLine($"Heapify, {string.Join("| ", a)}");
+            }
+        }
+
+        // Heapify to build a max-heap, places the highest value as the root of the tree
+        private static void Heapify(int[] a, int numberOfElements, int indexOfRoot)
+        {
+            int maxIndex = indexOfRoot; // sets the max index as the root index
+            int leftChildIndex = 2 * indexOfRoot + 1; // gets the left element index
+            int rightChildIndex = 2 * indexOfRoot + 2; // gets the right element index
+
+            // checks if the left leaf element value is greather than the max element value
+            maxIndex = leftChildIndex < numberOfElements && 
+                a[leftChildIndex] > a[maxIndex] ? leftChildIndex : maxIndex;
+
+            // checks if the right leaf element value is greather than the max element value
+            maxIndex = rightChildIndex < numberOfElements && 
+                a[rightChildIndex] > a[maxIndex] ? rightChildIndex : maxIndex;
+
+            if(maxIndex != indexOfRoot) // when the max has changed, is not the root anymore
+            {
+                (a[indexOfRoot], a[maxIndex]) = (a[maxIndex], a[indexOfRoot]); // swap elements, max element is placed at the root
+                Heapify(a, numberOfElements, maxIndex); // build a max-heap from location at which the new max index was found
+            }
+        }
+    }
+}
--- 03-SortingAlgorithms/Program.cs
+++ 03-SortingAlgorithms/Program.cs
@@ -1,37 +1,56 @@
 ﻿using _03_SortingAlgorithms;
 
+int[] arrayOrg = [-11, 12, -42, 0, 90, -9];
 //int[] arrayOrg = [-11, 12, -42, 0, 1, 90, 68, 6, -9];
-int[] arrayOrg = [-11, 12, 13, -15, -4, -9, -13];
+//int[] arrayOrg = [-11, 12, 13, -15, -4, -9, -13];
 
 int[] array1 = new int[arrayOrg.Length];
 int[] array2 = new int[arrayOrg.Length];
 int[] array3 = new int[arrayOrg.Length];
 int[] array4 = new int[arrayOrg.Length];
 int[] array5 = new int[arrayOrg.Length];
+int[] array6 = new int[arrayOrg.Length];
+int[] array7 = new int[arrayOrg.Length];
 
 Array.Copy(arrayOrg, array1, arrayOrg.Length);
 Array.Copy(arrayOrg, array2, arrayOrg.Length);
 Array.Copy(arrayOrg, array3, arrayOrg.Length);
 Array.Copy(arrayOrg, array4, arrayOrg.Length);
 Array.Copy(arrayOrg, array5, arrayOrg.Length);
+Array.Copy(arrayOrg, array6, arrayOrg.Length);
+Array.Copy(arrayOrg, array7, arrayOrg.Length);
 
 Console.WriteLine("Selection Sort");
 SelectionSort.Sort(array1);
-Console.WriteLine($"Original array:, {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array1)}");
 
 Console.WriteLine("Insertion Sort");
 InsertionSort.Sort(array2);
-Console.WriteLine($"Original array:, {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array2)}");
 
 Console.WriteLine("Bubble Sort");
 BubbleSort.Sort(array3);
-Console.WriteLine($"Original array:, {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array3)}");
 
 Console.WriteLine("Merge Sort");
 MergeSort.Sort(array4);
-Console.WriteLine($"Original array:, {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array4)}");
 
 Console.WriteLine("Shell Sort");
-Console.WriteLine($"Original array:, {string.Join("| ", arrayOrg)}");
 ShellSort.Sort(array5);
-Console.WriteLine($"Original array:, {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array5)}");
+
+Console.WriteLine("Quick Sort");
+QuickSort.Sort(array6);
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array6)}");
+
+Console.WriteLine("Heap Sort");
+HeapSort.Sort(array7);
+Console.WriteLine($"Original array: {string.Join("| ", arrayOrg)}");
+Console.WriteLine($"Sorted array: {string.Join("| ", array7)}");
